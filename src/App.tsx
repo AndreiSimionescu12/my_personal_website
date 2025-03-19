0c7d2dd5dcf81f0fbd8650b90e7eadfe4e2de3b5
AndreiSimionescu12 <andreisimionescu2000@gmail.com>
2025-03-19 12:06:57 +0200

    imp docker file

--- src/App.tsx
+++ src/App.tsx
@@ -138,7 +138,7 @@ function App() {
                 <div className="relative w-48 h-48 mx-auto mb-8">
                   <div className="absolute inset-0 bg-gradient-to-r from-purple-500 to-pink-500 rounded-full animate-pulse"></div>
                   <img 
-                    src="/images/profile.jpg" 
+                    src="images/profile.jpg" 
                     alt="Andrei Simionescu" 
                     className="relative w-full h-full object-cover rounded-full border-4 border-gray-900"
                   />
